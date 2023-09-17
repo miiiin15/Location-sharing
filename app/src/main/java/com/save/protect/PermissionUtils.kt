d20a439bfb910c2d421efe2f3cb714fbdc38778b
MintaeKim <gg04253@gmail.com>
2023-09-17 22:57:00 +0900

    Update PermissionUtils.kt : 이름 수정.

--- app/src/main/java/com/save/protect/PermissionUtils.kt
+++ app/src/main/java/com/save/protect/PermissionUtils.kt
@@ -18,7 +18,7 @@ object PermissionUtils {
     }
 
     // 위치 권한 설명이 필요한지 확인
-    fun shouldShowLocationPermissionRationale(activity: FragmentActivity): Boolean {
+    fun checkShowLocationPermission(activity: FragmentActivity): Boolean {
         return ActivityCompat.shouldShowRequestPermissionRationale(
             activity,
             android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -26,15 +26,16 @@ object PermissionUtils {
     }
 
     // 위치 권한 설명 다이얼로그 표시
-    fun showLocationPermissionExplanationDialog(activity: FragmentActivity) {
+    fun showLocationPermissionDialog(activity: FragmentActivity) {
         AlertDialog.Builder(activity)
             .setTitle("위치 권한 필요")
-            .setMessage("이 작업을 수행하기 위해 위치 권한이 필요합니다.")
+            .setMessage("이 작업을 수행하기 위해 위치 권한이 필요합니다.\n 미동의시 뒤로이동합니다.")
             .setPositiveButton("동의") { _, _ ->
                 requestLocationPermission(activity)
             }
             .setNegativeButton("취소") { dialog, _ ->
                 dialog.dismiss()
+                activity.finish()
             }
             .create()
             .show()
