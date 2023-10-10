422723787ff808aa6b9d0432f3b806d71622a51c
MintaeKim <gg04253@gmail.com>
2023-10-10 09:49:22 +0900

    Update CustomInput.kt : isSingleLine 설정

--- app/src/main/java/com/save/protect/custom/CustomInput.kt
+++ app/src/main/java/com/save/protect/custom/CustomInput.kt
@@ -34,6 +34,8 @@ class CustomInput : AppCompatEditText {
     private fun init() {
         // EditText의 배경을 투명한 밑줄로 설정
         background = ContextCompat.getDrawable(context, R.drawable.custom_input_bg)
+        // 1줄만
+        isSingleLine = true
 
 
         // 입력 중, 올바른 값, 올바르지 않은 값에 따라 밑줄 색상을 변경
