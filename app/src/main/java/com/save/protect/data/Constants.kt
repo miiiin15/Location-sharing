1a537dcedbbe68f139d704cee673a8d593c3fc85
MintaeKim <gg04253@gmail.com>
2024-02-22 10:39:27 +0900

    Fix : SERVER_URL 변경.

--- app/src/main/java/com/save/protect/data/Constants.kt
+++ app/src/main/java/com/save/protect/data/Constants.kt
@@ -2,21 +2,13 @@ package com.save.protect.data
 
 import android.content.Context
 
-/*
- * [Checklist]
- * 1. 운영서버 확인
- * 2. todo 코드 확인
- */
 object Constants {
 
     var context: Context? = null
 
 //    운영서버
-//    const val SERVER_URL = "https://loan.hbsb.co.kr:20010/"
 
-    //http://218.234.100.25:20020/api/test
     //개발서버
-    const val SERVER_URL = "http://218.234.100.25:20020/"
-
+    const val SERVER_URL = "http://210.115.230.154:59020/"
 
 }
