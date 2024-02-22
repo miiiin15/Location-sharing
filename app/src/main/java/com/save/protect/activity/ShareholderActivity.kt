81139cf392b6344b10250981bed3e9827b9ab4ed
MintaeKim <gg04253@gmail.com>
2024-02-22 10:54:16 +0900

    Update : 위치 저장 api 변경.

--- app/src/main/java/com/save/protect/activity/ShareholderActivity.kt
+++ app/src/main/java/com/save/protect/activity/ShareholderActivity.kt
@@ -5,11 +5,12 @@ import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import com.google.android.gms.location.*
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
@@ -21,9 +22,11 @@ import com.naver.maps.map.util.MarkerIcons
 import com.save.protect.BaseActivity
 import com.save.protect.R
 import com.save.protect.custom.BottomSheetChat
+import com.save.protect.data.LocationItem
 import com.save.protect.data.UserInfo
 import com.save.protect.data.UserManagement
-import com.save.protect.database.LocationTransmitter
+import com.save.protect.data.location.repo.LocationRepo
+import com.save.protect.helper.Logcat
 import com.save.protect.util.ImageUtils
 import com.save.protect.util.KakaoUtils
 import com.save.protect.util.PermissionUtils.checkShowLocationPermission
@@ -44,7 +47,6 @@ class ShareholderActivity : BaseActivity() {
     private lateinit var checkboxAutoFocus: CheckBox
 
     private lateinit var userData: UserInfo
-    private var uid: String? = null
     private var isAutoFocus = true
     private var marker = Marker()
 
@@ -56,7 +58,7 @@ class ShareholderActivity : BaseActivity() {
 
 
     // 위치 데이터를 저장하는 리스트 (제한된 길이 유지)
-    private val locationDataList = mutableListOf<Any>()
+    private val locationDataList = mutableListOf<LocationItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -95,7 +97,14 @@ class ShareholderActivity : BaseActivity() {
 
 
         button_invite.setOnClickListener {
-            KakaoUtils.shareText(this, "위치공유 초대", "위치공유 초대가 왔습니다.", "${uid}")
+            userData.uuid.let {
+                KakaoUtils.shareText(
+                    this,
+                    "위치공유 초대",
+                    "위치공유 초대가 왔습니다.",
+                    userData.uuid
+                )
+            }
         }
 
         checkboxAutoFocus.setOnCheckedChangeListener { buttonView, isChecked ->
@@ -148,11 +157,9 @@ class ShareholderActivity : BaseActivity() {
         val markLimit = intent.getIntExtra("MARK_LIMIT", 3)
         val updateInterval = intent.getIntExtra("UPDATE_INTERVAL", 10)
         val minimumInterval = intent.getIntExtra("MINIMUM_INTERVAL", 3)
-        uid = UserManagement.uid
         userData = UserManagement.getUserInfo()!!
 
-        Log.e("저장된유저 ", "${UserManagement.getUserInfo()}")
-        Log.e("저장된유저 ", UserManagement.uid)
+        Log.d("저장된유저 ", "${UserManagement.getUserInfo()}")
 
         // 값이 null인 경우 처리
         if (markLimit != 0 || updateInterval != 0 || minimumInterval != 0) {
@@ -187,15 +194,29 @@ class ShareholderActivity : BaseActivity() {
         }
     }
 
-    private fun _checkList(locationData: Any): MutableList<Any> {
+    private fun _checkList(latitude: Double, longitude: Double): String {
+        // 위치 데이터를 JSON 배열로 변환할 리스트
+        val locationDataList = mutableListOf<Pair<Double, Double>>()
+
         // 위치 데이터를 리스트에 추가
-        locationDataList.add(0, locationData)
-        // 리스트가 제한된 길이를 초과하는 경우, 가장 오래된 데이터를 제거 (선입선출)
+        locationDataList.add(0, Pair(latitude, longitude))
+
+        // 리스트의 길이가 설정된 한계를 초과하는 경우 가장 오래된 데이터를 제거 (FIFO)
+        val setting_markLimit = 10 // 예시로 설정된 한계 값
         if (locationDataList.size > setting_markLimit) {
             locationDataList.removeAt(setting_markLimit)
         }
 
-        return locationDataList
+        // 위치 데이터를 JSON 배열로 매핑
+        val jsonArray = locationDataList.map { (lat, lng) ->
+            val jsonObject = JsonObject()
+            jsonObject.addProperty("latitude", lat)
+            jsonObject.addProperty("longitude", lng)
+            jsonObject
+        }
+
+        // JSON 배열을 문자열로 변환하여 반환
+        return Gson().toJson(jsonArray)
     }
 
     private fun createLocationCallback() {
@@ -205,7 +226,24 @@ class ShareholderActivity : BaseActivity() {
                 for (location in locationResult.locations) {
                     // 위치 변경 시 작업
                     setMapMarker(location.latitude, location.longitude)
-                    uid?.let { LocationTransmitter.sendLocationData(it, _checkList(location)) }
+                    LocationRepo.saveLocationData(
+                        _checkList(location.latitude, location.longitude),
+                        success = {
+                            if (it.status == "OK") {
+                                Logcat.d("성공!")
+                            } else {
+                                showAlert(it.error.message.toString())
+                            }
+                        },
+                        failure = {
+                            loadingDialog.dismiss()
+                            showAlert(it.message.toString())
+                        },
+                        networkFail = {
+                            loadingDialog.dismiss()
+                            showAlert("$it : " + getString(R.string.network_error_message))
+                        }
+                    )
                     // 위치 변경을 출력
                     Log.d("위치 추적", "위도: ${location.latitude}, 경도: ${location.longitude}")
                 }
@@ -253,10 +291,10 @@ class ShareholderActivity : BaseActivity() {
 
             marker.position = initialLatLng
             marker.map = naverMap
-            if (userData.userName.isNotEmpty()) {
-                marker.captionText = userData.userName
+            if (userData.name.isNotEmpty()) {
+                marker.captionText = userData.name
             } else {
-                marker.captionText = "익명의 유저"
+                marker.captionText = getString(R.string.none_member_name)
             }
             if (userData.imageUrl.isNotEmpty()) {
                 // 마커 이미지를 URL에서 로드하여 설정
