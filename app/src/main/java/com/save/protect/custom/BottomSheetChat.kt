27e48c10dcc4431b769fe10c85d77a72dda3b2b6
MintaeKim <gg04253@gmail.com>
2024-09-10 21:28:15 +0900

    Refactor: 커스텀 바텀 시트 기능 구현을 위한 구조 수정 및 이미지 추가

--- app/src/main/java/com/save/protect/custom/BottomSheetChat.kt
+++ app/src/main/java/com/save/protect/custom/BottomSheetChat.kt
@@ -1,31 +1,79 @@
 package com.save.protect.custom
 
-import android.view.View
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat.getSystemService
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.save.protect.R
 
-object BottomSheetChat {
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+interface ButtonListener {
+    fun callback()
+}
 
-    // 바텀시트 초기화
-    fun initBottomSheet(
-        bottomSheet: View,
-        onChange: (bottomSheet: View, newState: Int) -> Unit,
-        onSlide: (bottomSheet: View, slideOffset: Float) -> Unit
-    ) {
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
 
-        bottomSheetBehavior.addBottomSheetCallback(object :
-            BottomSheetBehavior.BottomSheetCallback() {
-            // bottom sheet의 상태값 변경
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                onChange(bottomSheet, newState)
+class BottomSheetChat @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private lateinit var sheetInput: CustomInput
+    private lateinit var buttonSend: ImageButton
+    private lateinit var textViewSheetTitle: TextView
+
+    private var buttonListener: ButtonListener? = null
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.bottom_sheet_chat, this, true)
+
+        // View 초기화
+        sheetInput = findViewById(R.id.sheet_input)
+        buttonSend = findViewById(R.id.button_send)
+        textViewSheetTitle = findViewById(R.id.textView_sheet_title) // TextView 초기화
+
+        sheetInput.setIsValidListener(object : IsValidListener {
+            override fun isValid(text: String): Boolean {
+                updateButtonImage(text.isNotEmpty())
+                return text.isNotEmpty()
             }
-            // BottomSheet가 스크롤될 때 호출
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                onSlide(bottomSheet, slideOffset)
-            }
+        })
+
+
+        buttonSend.setOnClickListener {
+            buttonListener?.callback()
+            sheetInput.setText("")
         }
-        )
 
+
+    }
+
+
+    fun setTitle(text: String) {
+        textViewSheetTitle.text = text
+    }
+
+    fun setButtonListener(callback: ButtonListener) {
+        buttonListener = callback
+    }
+
+    fun moveFocusInput() {
+        sheetInput.requestFocus()
+    }
+
+    fun getMessage(): String {
+        return sheetInput.text.toString()
+    }
+
+    fun updateButtonImage(validate: Boolean) {
+        if (validate) {
+            buttonSend.setImageResource(R.drawable.icon_check) // 활성화된 이미지 리소스
+        } else {
+            buttonSend.setImageResource(R.drawable.icon_check_none) // 비활성화된 이미지 리소스
+        }
     }
 }
